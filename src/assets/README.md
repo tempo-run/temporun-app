e618a51af0137b45a803189648e6ac1f7dc8376f
Michel Diego Ribeiro Costa <micheldiegoti@outlook.com>
2026-05-13 06:03:29 +0100

    init: TempoRun app - Supabase auth + Google OAuth

---
+++ src/assets/README.md
@@ -0,0 +1,6 @@
+Coloque aqui os arquivos de imagem:
+- logo.png
+- runner_vertical.png
+- runner_stride.png
+- runner_landing.png
+- runner_overstride.png
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TempoRunApp from './TempoRun.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TempoRunApp />
+  </React.StrictMode>,
+)
